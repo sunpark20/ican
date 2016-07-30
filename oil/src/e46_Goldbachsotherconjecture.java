e774c6562ee82258bc2a44ba8cdd686cbf73b821
sunpark20 <sun.park20@gmail.com>
2016-07-31 01:25:40 +0900

    2016.07.31

--- oil/src/e46_Goldbachsotherconjecture.java
+++ oil/src/e46_Goldbachsotherconjecture.java
@@ -1,41 +1,64 @@
-// it's hard that made loop for two conditions.
+//T.T... IT IS TRASH...
+// x.x sleep mode coding
 
 public class e46_Goldbachsotherconjecture {
-
-    final static int PMAX=10000+1;
+    final static int NUMMAX=1000000;
+    final static int PMAX=NUMMAX+1;
+    final static int DSMAX=4;
     static boolean[] pArray=new boolean[PMAX];
     static int[] pArray_compressed=new int[PMAX/2];
 
+
     public static void main(String[] args){
         makePrimeArray();
-        //target = 1 제외, 소수 제외
-        for(int target=3;target<PMAX;target++){
-            if(target%2==0 || pArray[target])
-                continue;
-            // < start outer while 소수를 관리한다.
-            int primeCount=0;
-            while (true){
-                boolean next=false;
+        int num=1;
 
-                int squreCount=1; // <start inner while 여기선 스퀘어를 계쏙 증가해준다.
-                while (true){
-                    int cValue=pArray_compressed[primeCount]+squreCount*squreCount*2;
-                    System.out.println(target+"  "+cValue+"="+pArray_compressed[primeCount]+" "+squreCount);
-                    if(target==cValue){
-                        System.out.println("ok " + target);
-                        next=true;
-                        break;
+        int temp=-1;
+        int rCount=0;
+        for(int j=num;j<NUMMAX;j++){
+            int cNum=j;
+            int i=0;
+            int pNum=0;
+            int memory=-1;
+
+            while (true){
+//                System.out.println(cNum+ " "+pArray_compressed[i]);
+                if(cNum%pArray_compressed[i]==0) {
+                    if(memory!=i) {
+                        memory = i;
+                        pNum++;
                     }
-                    if(target<cValue)
-                        break;
-                    squreCount++;
-                }  // >end inner while
-                if(next)
+                    cNum = cNum / pArray_compressed[i];
+                } else {
+                    i++;
+                }
+                if(cNum==1 && pNum==DSMAX) {
+                    if(temp==-1) {
+                        temp = j;
+                    }else{
+                        if(j-temp==1){
+                            temp=j;
+                            rCount++;
+                        }else{
+                            temp=-1;
+                            rCount=0;
+                        }
+                    }
+                    if(rCount==DSMAX-1){
+                        System.out.println("빙고");
+                        System.exit(1);
+                    }
+
+                    System.out.println(j+" "+DSMAX+"번 나눴어요");
                     break;
-                primeCount++;
-            } // > end outer while
+                }
+                if(cNum < pArray_compressed[i] || DSMAX < pNum ){
+                    temp=-1;
+                    rCount=0;
+                    break;
+                }
+            }
         }
-        // 멈춤조건  target < prime + 2*(1^2)
 
     }
 
@@ -62,22 +85,6 @@ public class e46_Goldbachsotherconjecture {
         for(int i=2;i<pArray.length;i++){
             if(pArray[i])
                 pArray_compressed[count++]=i;
-        }
-    }
-
-    static void showPArray(){
-        for(int i=1;i<pArray.length;i++){
-            if(pArray[i])
-                System.out.println(i);
-        }
-    }
-
-    static void showComp(){
-        int count=0;
-        while(true){
-            if(pArray_compressed[count]==0)
-                break;
-            System.out.println(pArray_compressed[count++]);
         }
     }
 }
